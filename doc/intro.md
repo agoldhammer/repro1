a26e44ddfecb862e1262678927115fea8add4fea
Art Goldhammer <art.goldhammer@gmail.com>
2021-12-22 16:03:26 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to repro1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/agoldhammer/repro1_test.clj
@@ -0,0 +1,7 @@
+(ns agoldhammer.repro1-test
+  (:require [clojure.test :refer :all]
+            [agoldhammer.repro1 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
